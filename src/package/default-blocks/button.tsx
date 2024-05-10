0fa5b2f51cf61f4a5ddd886835bf48522bb5e603
Suraj Air <surajair@gmail.com>
2024-05-10 08:53:25 +0530

    fix: button icon missing

--- src/package/default-blocks/button.tsx
+++ src/package/default-blocks/button.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ButtonIcon } from "@radix-ui/react-icons";
-import { Icon, Link, SelectOption, SingleLineText, Styles } from "../controls";
+import { Icon, Link, SelectOption, Numeric, SingleLineText, Styles } from "../controls";
 import { registerChaiBlock } from "../runtime/builder-blocks";
 import { ChaiBlock } from "../helper/types/ChaiBlock.ts";
 
@@ -10,12 +10,22 @@ const ButtonBlock = (
     styles: Record<string, string>;
   },
 ) => {
-  const { blockProps, icon, content, iconPos, styles, children } = block;
-
+  const { blockProps, iconSize, icon, content, iconPos, styles, children } = block;
+  let _icon = icon;
+  // add width height attr to icon
+  if (icon) {
+    const iconElement = document.createElement("div");
+    iconElement.innerHTML = icon;
+    const svg = iconElement.querySelector("svg");
+    if (svg) {
+      svg.setAttribute("width", iconSize + "px");
+    }
+    _icon = svg.outerHTML;
+  }
   const child = children || (
     <>
       {content}
-      {icon && <span className={iconPos || ""} dangerouslySetInnerHTML={{ __html: icon }} />}
+      {_icon && <span className={iconPos + " mx-2 h-full w-full" || ""} dangerouslySetInnerHTML={{ __html: _icon }} />}
     </>
   );
   return React.createElement("button", { ...blockProps, ...styles, type: "button" }, child);
@@ -28,19 +38,11 @@ registerChaiBlock(ButtonBlock as React.FC<any>, {
   icon: ButtonIcon,
   group: "basic",
   props: {
-    content: SingleLineText({
-      title: "Label",
-      default: "Button",
-      multiLingual: true,
-    }),
-    styles: Styles({
-      default: "text-white bg-primary px-4 py-2 rounded-global flex items-center",
-    }),
-    link: Link({
-      title: "Link",
-      default: { type: "page", href: "", target: "_blank" },
-    }),
+    content: SingleLineText({ title: "Label", default: "Button", multiLingual: true }),
+    styles: Styles({ default: "text-white bg-primary px-4 py-2 rounded-global flex items-center" }),
+    link: Link({ title: "Link", default: { type: "page", href: "", target: "_blank" } }),
     icon: Icon({ title: "Icon", default: "" }),
+    iconSize: Numeric({ title: "Icon Size", default: 24 }),
     iconPos: SelectOption({
       title: "Icon Position",
       default: "order-last",
