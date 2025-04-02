4043b0af1b46eccd04f105a3e0a0143b9930a4b2
Suraj Air <surajair@gmail.com>
2025-04-02 13:29:57 +0530

    refactor: update font types to include fallback property and rename font retrieval parameters

--- src/package/v2/runtime/fonts.ts
+++ src/package/v2/runtime/fonts.ts
@@ -1,92 +1,95 @@
 export type ChaiGoogleFont = {
-  name: string;
-  url: string;
   family: string;
+  url: string;
+  fallback: string;
 };
 
 export type ChaiCustomFont = {
-  name: string;
-  src: { url: string; format: string }[];
   family: string;
-  fontWeight?: string;
-  fontStyle?: string;
-  fontDisplay?: string;
-  fontStretch?: string;
+  src: {
+    url: string;
+    format: string;
+    fontWeight?: string;
+    fontStyle?: string;
+    fontDisplay?: string;
+    fontStretch?: string;
+  }[];
+  fallback: string;
 };
 
 export type ChaiFont = ChaiGoogleFont | ChaiCustomFont;
 
 const REGISTERED_FONTS: ChaiFont[] = [
   {
-    name: "Roboto",
+    family: "Roboto",
     url: "https://fonts.googleapis.com/css2?family=Roboto:ital,wdth,wght@0,75..100,100..900;1,75..100,100..900&display=swap",
-    family: `"Roboto", sans-serif`,
+    fallback: `"Roboto", sans-serif`,
   },
   {
-    name: "Inter",
+    family: "Inter",
     url: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap",
-    family: `"Inter", sans-serif`,
+    fallback: `"Inter", sans-serif`,
   },
   {
-    name: "DM Sans",
+    family: "DM Sans",
     url: "https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap",
-    family: `"DM Sans", sans-serif`,
+    fallback: `"DM Sans", sans-serif`,
   },
   {
-    name: "Open Sans",
+    family: "Open Sans",
     url: "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap",
-    family: `"Open Sans", sans-serif`,
+    fallback: `"Open Sans", sans-serif`,
   },
   {
-    name: "Lato",
+    family: "Lato",
     url: "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap",
-    family: `"Lato", sans-serif`,
+    fallback: `sans-serif`,
   },
   {
-    name: "Montserrat",
+    family: "Montserrat",
     url: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
-    family: `"Montserrat", sans-serif`,
+    fallback: `sans-serif`,
   },
   {
-    name: "Poppins",
+    family: "Poppins",
     url: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
-    family: `"Poppins", sans-serif`,
+    fallback: `sans-serif`,
   },
   {
-    name: "Source Sans 3",
+    family: "Source Sans 3",
     url: "https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap",
-    family: `"Source Sans 3", sans-serif`,
+    fallback: `sans-serif`,
   },
   {
-    name: "Nunito Sans",
+    family: "Nunito Sans",
     url: "https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap",
-    family: `"Nunito Sans", sans-serif`,
+    fallback: `sans-serif`,
   },
   {
-    name: "Oswald",
+    family: "Oswald",
     url: "https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap",
-    family: `"Oswald", sans-serif`,
+    fallback: `sans-serif`,
   },
   {
-    name: "Raleway",
+    family: "Raleway",
     url: "https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap",
-    family: `"Raleway", sans-serif`,
+    fallback: `sans-serif`,
   },
   {
-    name: "Merriweather",
+    family: "Merriweather",
     url: "https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap",
-    family: `"Merriweather", serif`,
+    fallback: `serif`,
   },
   {
-    name: "Playfair Display",
+    family: "Playfair Display",
     url: "https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap",
-    family: `"Playfair Display", serif`,
+    fallback: `serif`,
   },
 ];
 
-export const registerChaiFont = (fontName: string, font: Omit<ChaiFont, "name">) => {
+export const registerChaiFont = (family: string, font: Omit<ChaiFont, "family">) => {
   REGISTERED_FONTS.unshift({
-    name: fontName,
+    family,
     ...font,
   } as ChaiFont);
 };
@@ -95,8 +98,8 @@ export const useRegisteredFonts = () => {
   return REGISTERED_FONTS;
 };
 
-export const getRegisteredFont = (fontName: string) => {
-  return REGISTERED_FONTS.find((font) => font.name === fontName);
+export const getRegisteredFont = (family: string) => {
+  return REGISTERED_FONTS.find((font) => font.family === family);
 };
 
 export const getAllRegisteredFonts = () => {
